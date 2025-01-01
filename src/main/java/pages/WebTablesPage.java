d7dccfe009d228c98e336fe5cfaf6452d8999c97
AshishKumar95 <as.k231216@gmail.com>
2025-01-01 14:32:07 +0530

    added braces

--- src/main/java/pages/WebTablesPage.java
+++ src/main/java/pages/WebTablesPage.java
@@ -11,13 +11,14 @@ public class WebTablesPage extends BaseLibrary {
     public WebTablesPage() {
         PageFactory.initElements(driver, this);
     }
+
     @FindBy(xpath = "//*[text()='×']")
     private WebElement close;
-    @FindBy(xpath="//a[@href='newdemo.html']")
+    @FindBy(xpath = "//a[@href='newdemo.html']")
     private WebElement practice;
-    @FindBy(xpath="//button[@data-target='#elements']")
+    @FindBy(xpath = "//button[@data-target='#elements']")
     private WebElement Element;
-    @FindBy(xpath ="//*[@href='#tab_4']")
+    @FindBy(xpath = "//*[@href='#tab_4']")
     private WebElement webtable;
     @FindBy(xpath = "//input[@pattern='^[a-zA-Z][\\sa-zA-Z]{2,32}']")
     private WebElement fullname;
@@ -27,58 +28,63 @@ public class WebTablesPage extends BaseLibrary {
     private WebElement submit;
     @FindBy(xpath = "//iframe[@src='Webtable.html']")
     private WebElement iframe;
-    @FindBy(xpath="class=//button[@class='btn btn-info btn-xs btn-edit']")
+    @FindBy(xpath = "class=//button[@class='btn btn-info btn-xs btn-edit']")
     private WebElement editbtn;
     @FindBy(xpath = "*//button[@class='btn btn-info btn-xs btn-update']")
     private WebElement updatebtn;
-    @FindBy(xpath="//input[@name='edit_name']")
+    @FindBy(xpath = "//input[@name='edit_name']")
     private WebElement editname;
-    @FindBy(xpath="//input[@name='edit_email']")
+    @FindBy(xpath = "//input[@name='edit_email']")
     private WebElement editemail;
     @FindBy(xpath = "*//table[@class='table table-bordered data-table']//tbody//td[1]")
     private List<WebElement> updatedname;
     @FindBy(xpath = "*//table[@class='table table-bordered data-table']//tbody//td[2]")
     private List<WebElement> updatedemail;
+
     // click on page elements
-    public void clickonclose() {wait(close);}
-    public void clickonpractice()
-    {
+    public void clickonclose() {
+        wait(close);
+    }
+
+    public void clickonpractice() {
         wait(practice);
     }
-    public void clickonelements()
-    {
+
+    public void clickonelements() {
         wait(Element);
     }
-    public void clickonwebtables(){wait(webtable);}
-    public void filldetails()
-    {
+
+    public void clickonwebtables() {
+        wait(webtable);
+    }
+
+    public void filldetails() {
         driver.switchTo().frame(iframe);
-        for(int i=1  ; i<4; i++) {
+        for (int i = 1; i < 4; i++) {
             visiblewait(fullname, getReadData(0, i, 0));
             visiblewait(email, getReadData(0, i, 1));
             wait(submit);
         }
 
 
-        public void validate()
+        public void validate ()
         {
-  System.outprintln("asdfghjkl");
+            System.outprintln("asdfghjkl");
         }
         driver.switchTo().defaultContent();
-       // driver.switchTo().defaultContent();
+        // driver.switchTo().defaultContent();
     }
-    public void updatedetails()
-    {
-        for(int i=1  ; i<4; i++) {
+
+    public void updatedetails() {
+        for (int i = 1; i < 4; i++) {
             wait(editbtn);
             editname.clear();
             visiblewait(editname, getReadData(1, i, 0));
             editemail.clear();
             visiblewait(editemail, getReadData(1, i, 1));
             wait(updatebtn);
-            System.out.println("this is for testbranch and not commitiing ");
 
 
+        }
     }
-
 }
